6aa683a7404423faf4066c1fcb53db9dbfb63752
KSHITIZ BHARGAV <kbgoku@gmail.com>
2023-12-03 21:56:30 +0530

    First commit with working server

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# CredHive
-CredHive Interview Project
+# FastAPI Credit Information API
+
+This FastAPI application provides a RESTful API for managing credit information. It includes CRUD operations, data validation, rate limiting, and token-based authentication.
+
+## Requirements
+
+- Python 3.7 or higher
+- Pip (Python package installer)
+
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/your-username/your-repo.git
+    cd your-repo
+    ```
+
+2. Create a virtual environment:
+
+    ```bash
+    python -m venv venv
+    ```
+
+3. Activate the virtual environment:
+
+    - On Windows:
+
+        ```bash
+        .\venv\Scripts\activate
+        ```
+
+    - On macOS/Linux:
+
+        ```bash
+        source venv/bin/activate
+        ```
+
+4. Install the required packages:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Database Setup
+
+1. Open a Python shell and run the following commands to create the SQLite database tables:
+
+    ```python
+    from sqlalchemy import create_engine, MetaData
+    from your_app import Base, DATABASE_URL
+
+    engine = create_engine(DATABASE_URL)
+    metadata = MetaData()
+
+    Base.metadata.create_all(bind=engine)
+    ```
+
+Replace `your_app` with the actual name of your FastAPI application file.
+
+## Running the Application
+
+1. Run the FastAPI application using Uvicorn:
+
+    ```bash
+    uvicorn main:app --reload
+    ```
+
+
+2. Open your web browser and navigate to `http://127.0.0.1:8000/docs` to access the Swagger UI for testing the API endpoints.
+
+## API Endpoints
+
+- **GET /credits**: Retrieve a list of all credit information.
+- **GET /credits/{id}**: Retrieve credit information for a specific user by ID.
+- **POST /credits**: Add new credit information.
+- **PUT /credits/{id}**: Update credit information for a specific user.
+- **DELETE /credits/{id}**: Delete credit information for a specific user.
+
+## Authentication
+
+- Basic token-based authentication is implemented. Use the provided "fake-token" for testing.
+
+## Rate Limiting
+
+- Rate limiting is applied to protect against abuse. The default rate limit is 100 requests per minute.
---
+++ main.py
@@ -0,0 +1,164 @@
+# main.py
+
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi_limiter.depends import RateLimiter
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Float, DateTime, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from pydantic import BaseModel
+from typing import List
+
+DATABASE_URL = "sqlite:///./credhive.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+metadata = MetaData()
+
+Base = declarative_base()
+
+# Define the SQLAlchemy model for credit information
+class CreditInfoModel(Base):
+    __tablename__ = "credit_info"
+
+    id = Column(Integer, primary_key=True, index=True)
+    company_name = Column(String, index=True)
+    address = Column(String)
+    registration_date = Column(DateTime)
+    number_of_employees = Column(Integer)
+    raised_capital = Column(Float)
+    turnover = Column(Float)
+    net_profit = Column(Float)
+    contact_number = Column(String)
+    contact_email = Column(String)
+    company_website = Column(String)
+    loan_amount = Column(Float)
+    loan_interest = Column(Float)
+    account_status = Column(Boolean)
+    # ... add other fields ...
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
+# Pydantic model for credit information
+class CreditInfo(BaseModel):
+    company_name: str
+    address: str
+    registration_date: str
+    number_of_employees: int
+    raised_capital: float
+    turnover: float
+    net_profit: float
+    contact_number: str
+    contact_email: str
+    company_website: str
+    loan_amount: float
+    loan_interest: float
+    account_status: bool
+    # ... add other fields ...
+
+# Pydantic model for credit information update
+class CreditInfoUpdate(BaseModel):
+    company_name: str = None
+    address: str = None
+    registration_date: str = None
+    number_of_employees: int = None
+    raised_capital: float = None
+    turnover: float = None
+    net_profit: float = None
+    contact_number: str = None
+    contact_email: str = None
+    company_website: str = None
+    loan_amount: float = None
+    loan_interest: float = None
+    account_status: bool = None
+    # ... add other fields ...
+
+# OAuth2 token for authentication
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Create the database session using sessionmaker
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Function to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Function to validate the token
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    # This function can be extended to validate the token against a user database
+    if token != "fake-token":
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return token
+
+app = FastAPI()
+
+# API endpoint to retrieve a list of all credit information
+@app.get("/credits", response_model=List[CreditInfo])
+async def get_all_credits(db: Session = Depends(get_db)):
+    credits = db.query(CreditInfoModel).all()
+    return credits
+
+# API endpoint to retrieve credit information for a specific user by ID
+@app.get("/credits/{id}", response_model=CreditInfo)
+async def get_credit_by_id(id: int, db: Session = Depends(get_db)):
+    credit_info = db.query(CreditInfoModel).filter(CreditInfoModel.id == id).first()
+    if credit_info is None:
+        raise HTTPException(status_code=404, detail="Credit information not found")
+    return credit_info
+
+# API endpoint to add new credit information
+@app.post("/credits", response_model=CreditInfo)
+async def add_credit_info(
+    credit_info: CreditInfo,
+    current_user: str = Depends(get_current_user),
+    rate_limiter: RateLimiter = Depends(),
+    db: Session = Depends(get_db),
+):
+    # Save the credit information to the database
+    db_credit_info = CreditInfoModel(**credit_info.dict())
+    db.add(db_credit_info)
+    db.commit()
+    db.refresh(db_credit_info)
+    return db_credit_info
+
+# API endpoint to update credit information for a specific user
+@app.put("/credits/{id}", response_model=CreditInfo)
+async def update_credit_info(
+    id: int,
+    credit_info_update: CreditInfoUpdate,
+    current_user: str = Depends(get_current_user),
+    rate_limiter: RateLimiter = Depends(),
+    db: Session = Depends(get_db),
+):
+    db_credit_info = db.query(CreditInfoModel).filter(CreditInfoModel.id == id).first()
+    if db_credit_info is None:
+        raise HTTPException(status_code=404, detail="Credit information not found")
+
+    # Update the credit information in the database
+    for field, value in credit_info_update.dict(exclude_unset=True).items():
+        setattr(db_credit_info, field, value)
+
+    db.commit()
+    db.refresh(db_credit_info)
+    return db_credit_info
+
+# API endpoint to delete credit information for a specific user
+@app.delete("/credits/{id}")
+async def delete_credit_info(
+    id: int,
+    current_user: str = Depends(get_current_user),
+    rate_limiter: RateLimiter = Depends(),
+    db: Session = Depends(get_db),
+):
+    db_credit_info = db.query(CreditInfoModel).filter(CreditInfoModel.id == id).first()
+    if db_credit_info is None:
+        raise HTTPException(status_code=404, detail="Credit information not found")
+
+    # Delete the credit information from the database
+    db.delete(db_credit_info)
+    db.commit()
+    return {"message": "Credit information deleted successfully"}
